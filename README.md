5dd96706ab31deb1c3bf2cda5c4edca031954242
padhend <padhend@hotmail.com>
2024-04-21 10:20:53 +0100

    Some improvements

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Scripts to create the most awsome POPOS! install
 
 This script will:
+Add an entry to the sudoers file
 
---
+++ root.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+##############################
+## If not root, become root ##
+##############################
+if [ $USER != "root" ]
+then
+  sudo su -
+  ## Add your account to the sudoers file, so you dont have to keep typing the password 
+  echo "neil ALL = (root) NOPASSWD : ALL" > /etc/sudoers.d/neil
+  exit
+fi
